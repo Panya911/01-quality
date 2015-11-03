cd1754b63838a003f7bb35e6fb9e88f4e1ffe871
Panya911 <Pavel_flyagin@icloud.com>
2015-11-03 20:31:57 +0500

    first step

---
+++ MarkDownTests/MarkDownProcessor_should.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Markdown;
+using NUnit.Framework;
+
+namespace MarkDownTests
+{
+    [TestFixture]
+    public class MarkDownProcessor_should
+    {
+        private readonly MarkDownProcessor _markDownProcessor;
+
+        public MarkDownProcessor_should()
+        {
+            var commands = new Dictionary<string, Command>
+            {
+                ["_"] = new Command("<em>", "</em>", true),
+                ["__"] = new Command("<strong>", "</strong>", true),
+                ["`"] = new Command("<code>", "</code>", false)
+            };
+            _markDownProcessor = new MarkDownProcessor(commands);
+        }
+
+        [Test]
+        public void NotChangeInputStringWithoutCommand()
+        {
+            var input = "Просто текст";
+            var result = _markDownProcessor.ProcessString(input);
+            var expected = input.Clone();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("Текст _окруженный с двух сторон_  одинарными символами подчерка",
+                    Result = "Текст <em>окруженный с двух сторон</em>  одинарными символами подчерка",
+                    TestName = "ReplaceSingleSnakes")]
+
+        [TestCase("Текст _окруженный с двух сторон_  одинарными _символами подчерка_",
+                    Result = "Текст <em>окруженный с двух сторон</em>  одинарными <em>символами подчерка</em>",
+                    TestName = "ReplaceMultipleSnakes")]
+
+        [TestCase("Текст \\_окруженный с двух сторон\\_ одинарными символами подчерка",
+                    Result = "Текст _окруженный с двух сторон_ одинарными символами подчерка",
+                    TestName = "NotReplaceScreenedSnake")]
+
+        [TestCase("Текст \\_окруженный с двух сторон\\_ одинарными \\_символами подчерка\\_",
+                    Result = "Текст _окруженный с двух сторон_ одинарными _символами подчерка_",
+                    TestName = "NotReplaceMultipleScreenedSnake")]
+
+        [TestCase("_Текст окруженный с двух сторон_ одинарными символами подчерка",
+                    Result = "<em>Текст окруженный с двух сторон</em> одинарными символами подчерка",
+                    TestName = "ReplaceSnakesAtFirstIndex")]
+
+        public string ReplaceSnakeToEm(string str)
+        {
+            return _markDownProcessor.ProcessString(str);
+        }
+
+        [TestCase("Двумя __символами__ - должен становиться жирным",
+            Result = "Двумя <strong>символами</strong> - должен становиться жирным",
+            TestName = "ReplaceSingleDoubleSnakes")]
+
+        [TestCase("Двумя __символами__ - должен __становиться__ жирным",
+            Result = "Двумя <strong>символами</strong> - должен <strong>становиться</strong> жирным",
+            TestName = "ReplaceMultipleDoubleSnakes")]
+
+        [TestCase("Двумя \\_\\_символами\\_\\_ - должен становиться жирным",
+            Result = "Двумя __символами__ - должен становиться жирным",
+            TestName = "NotReplaceSimpleScreenedDoubleSnakes")]
+
+        [TestCase("Двумя \\_\\_символами\\_\\_ - должен \\_\\_становиться\\_\\_ жирным",
+            Result = "Двумя __символами__ - должен __становиться__ жирным",
+            TestName = "NotReplaceSimpleScreenedDoubleSnakes")]
+
+        [TestCase("__Двумя символами__ - должен становиться жирным",
+            Result = "<strong>Двумя символами</strong> - должен становиться жирным",
+            TestName = "ReplaceDoubleSnakesAtFirstIndex")]
+
+        public string ReplaceDoubleSnakesToStrong(string str)
+        {
+            return _markDownProcessor.ProcessString(str);
+        }
+
+        [Test]
+        public void NotReplaceTags_IntoCodeTag()
+        {
+            var input = "Текст окруженный `одинарными _обратными_ кавычками` (backticks)";
+            var expected = "Текст окруженный <code>одинарными _обратными_ кавычками</code> (backticks)";
+            var result = _markDownProcessor.ProcessString(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ThrowException_OnBadInput()
+        {
+            var input = "Плохой _текст";
+            Assert.Throws<ArgumentException>(() => _markDownProcessor.ProcessString(input));
+        }
+    }
+}
---
+++ Markdown/MarkDownProcessor.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Markdown
+{
+    public class Command
+    {
+        public readonly string OpenedForm;
+        public readonly string ClosedForm;
+        public readonly bool NeedFormateInto;
+
+        public Command(string openedForm, bool needFormatInto)
+        {
+            OpenedForm = openedForm;
+            ClosedForm = null;
+            NeedFormateInto = needFormatInto;
+        }
+
+        public Command(string openedForm, string closedForm, bool needFormatInto)
+        {
+            OpenedForm = openedForm;
+            ClosedForm = closedForm;
+            NeedFormateInto = needFormatInto;
+        }
+
+        public bool IsCloseable => ClosedForm != null;
+    }
+
+
+    public class MarkDownProcessor
+    {
+        private readonly CommandTranslater _translater;
+        public MarkDownProcessor(Dictionary<string, Command> commands)
+        {
+            _translater = new CommandTranslater(commands);
+        }
+
+        public string HanldeBlock(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ProcessString(string str)
+        {
+            var result = new StringBuilder();
+            var openedTags = new Stack<string>();
+            var temp = "";
+            var needFormat = true;
+            for (var i = 0; i < str.Length; i++)
+            {
+                temp += str[i];
+                if (temp == "\\")
+                {
+                    result.Append(str[i + 1]);
+                    i++;
+                    temp = "";
+                    continue;
+                }
+                var answer = _translater.CanDefineCommand(temp);
+                switch (answer)
+                {
+                    case CommandTranslater.Answer.No:
+                        result.Append(temp);
+                        temp = "";
+                        break;
+                    case CommandTranslater.Answer.Yes:
+                        {
+                            var cmd = TranslateCommandToNeedForm(temp, openedTags);
+                            if (!_translater.NeedFormatInto(temp))
+                            {
+                                needFormat = !needFormat;
+                                result.Append(cmd);
+                            }
+                            else
+                                result.Append(needFormat ? cmd : temp);
+                            temp = "";
+                            break;
+                        }
+
+                    case CommandTranslater.Answer.YesButCanBeLonger:
+                        {
+                            //todo выпилить костыль
+                            if (i + 1 == str.Length ||
+                                _translater.CanDefineCommand(temp + str[i + 1]) == CommandTranslater.Answer.No)
+                            {
+                                var cmd = TranslateCommandToNeedForm(temp, openedTags);
+                                result.Append(needFormat ? cmd : temp);
+                                temp = "";
+                            }
+                            break;
+                        }
+                    case CommandTranslater.Answer.Maybe:
+                        {
+                            continue;
+                        }
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            if (openedTags.Count!=0)
+                throw new ArgumentException("Ваш текст отстой:)");
+            return result.ToString();
+        }
+
+        private string TranslateCommandToNeedForm(string command, Stack<string> openedTags)
+        {
+            if (!_translater.IsCloseableCommand(command))
+                return _translater.GetOpenedForm(command);
+
+            if (openedTags.Count > 0 && openedTags.Peek() == command)
+                return _translater.GetClosedForm(openedTags.Pop());
+            openedTags.Push(command);
+            return _translater.GetOpenedForm(command);
+        }
+
+        private class CommandTranslater
+        {
+            private readonly Dictionary<string, Command> _commands;
+
+            public CommandTranslater(Dictionary<string, Command> commands)
+            {
+                _commands = commands.Clone();
+            }
+            public enum Answer
+            {
+                Yes,
+                YesButCanBeLonger,
+                No,
+                Maybe
+            }
+
+            public Answer CanDefineCommand(string str)
+            {
+                var c = _commands.Keys.Where(key => key.Contains(str)).ToList();
+                if (c.Count > 1)
+                    return _commands.ContainsKey(str)
+                        ? Answer.YesButCanBeLonger
+                        : Answer.Maybe;
+                if (c.Count == 1)
+                    return c.First() == str ? Answer.Yes : Answer.Maybe;
+                return Answer.No;
+            }
+
+            public string GetClosedForm(string command)
+            {
+                return _commands[command].ClosedForm;
+            }
+
+            public bool NeedFormatInto(string command)
+            {
+                return _commands[command].NeedFormateInto;
+            }
+            public bool IsCloseableCommand(string str)
+            {
+                return _commands[str].IsCloseable;
+            }
+
+            public string GetOpenedForm(string command)
+            {
+                return _commands[command].OpenedForm;
+            }
+        }
+    }
+
+    public static class DictionaryExtension
+    {
+        public static Dictionary<TKey, TValue> Clone<TKey, TValue>(this Dictionary<TKey, TValue> dict)
+        {
+            return dict.ToDictionary(pair => pair.Key, pair => pair.Value);
+        }
+    }
+}
